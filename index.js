c2196744a0ec54b54b2258aa687f3bb44397454d
Will Elmer <willpelmer12@gmail.com>
2023-05-26 11:48:09 -0500

    Final commit

--- index.js
+++ index.js
@@ -16,36 +16,55 @@
           modified copy
 */
 
-
 // create our pizza as an empty object
-const pizza = {  }
+const pizza = {};
 
 //create a function to add size to pizza
+// add a size property
+
 const addSize = (sizeOfPizza) => {
-      // add a size property
-      pizza.size = sizeOfPizza
-}
+  pizza.size = sizeOfPizza;
+};
+
 // create a function to add toppings to our pizza
 // add 2 parameters for each topping
-const addToppings = (topping1, topping2) => {
-      // add topping property to pizza object
-      pizza.toppings = [topping1, topping2]
-}
-addSize("small")
-addToppings("cheese", "pepperoni")
-console.log(pizza)
-// create a function to bake pizza
-const bakePizza = () => {
-      // add a baked property with a boolean value
+// add topping property to pizza object
 
-}
+const addToppings = (topping1, topping2) => {
+  pizza.toppings = [topping1, topping2];
+};
+addSize("large");
+addToppings("cheese", "pepperoni");
+
+// create a function to bake pizza
+// add a baked property with a boolean value
+
+const bakePizza = () => {
+  pizza.baked = true;
+};
+bakePizza();
 
 // create a function to mark as paid
+// add a paid property with a boolean value
+
 const isPaid = () => {
+  pizza.paid = true;
+};
+isPaid();
 
-}
 // create a function to check if paid for and return HTML
+// create an if statement to check if pizza is paid for
+// if pizza is paid for console log "Your {pizza details} is being delivered"
+// if pizza is not paid console log "Please pay us first"
+
 const deliverPizza = () => {
+  if (pizza.paid === true) {
+    console.log(
+      `Your ${pizza.size} ${pizza.toppings[0]} and ${pizza.toppings[1]} pizza is being delivered`
+    );
+  } else {
+    console.log("Please pay us first.");
+  }
+};
 
-}
-
+deliverPizza();
